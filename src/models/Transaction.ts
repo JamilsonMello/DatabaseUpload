e8a4eda242aa7abec26dd2cb4948da21dde977f5
Gyasmin <gyasminassuncao@uni9.edu.br>
2020-04-26 01:21:24 -0300

    Configurando a conexão entre as migrations e os models

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,39 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity('transaction')
 class Transaction {
+  @PrimaryGeneratedColumn()
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('decimal')
   value: number;
 
+  @ManyToOne(() => Category)
+  @JoinColumn()
+  category: Category;
+
+  @Column()
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
